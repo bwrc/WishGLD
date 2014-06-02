78b875f96ebf6adcfd2886bc93ce25a7ea94628b
kride <klukander@gmail.com>
2014-06-02 13:07:22 +0300

    cleaned up stimgen

--- halftone_stim_generation.py
+++ halftone_stim_generation.py
@@ -15,10 +15,8 @@ myMon=monitors.Monitor('yoga', width=29.3, distance=40); myMon.setSizePix((3200,
 #desktop
 #myMon=monitors.Monitor('asus', width=37.8, distance=40); myMon.setSizePix((1920, 1080))
 win = visual.Window( size=(IMG_W, IMG_H),units='pix',fullscr=False,monitor=myMon, color=(1.0, 1.0, 1.0), colorSpace='rgb')
-#win = visual.Window( size=(IMG_W, IMG_H),units='pix',fullscr=False,monitor=myMon, color=(0.0, 0.0, 0.0), colorSpace='rgb')
 
 faces = []
-#faces.append( Image.open('C:\\Kride\\Projects\\ReKnow\\WCST\\letters\\noiseA.png' ) );
 
 #faces
 #for i in range(17):
@@ -28,31 +26,12 @@ faces = []
 for i in range(17):
     faces.append( Image.open('C:\\Kride\\Projects\\ReKnow\\WCST\\shinedef1by1\\ltr' + '%01d' % (i+1,) + ' (Custom).png') )
 
-#face1 = Image.open('pics\\natalie26.png')
-#face2 = Image.open('pics\\girl1.png')
-#face3 = Image.open('pics\\girl2.png')
-#face4 = Image.open('pics\\ben.png')
-#face5 = Image.open('pics\\letter_S.png')
-#face6 = Image.open('pics\\letter_S_noise.png')
-#face7 = Image.open('pics\\letter_S_noise2.png')
-
-#facetest = Image.open( 'C:\\Kride\\Projects\\ReKnow\\pics\\faces_ATTK\\26x26\\f01.png' )
-#faces.append( facetest )
-
-#faces.append( face1 )
-#faces.append( face2 )
-#faces.append( face3 )
-#faces.append( face4 )
-#faces.append( face5 )
-#faces.append( face6 )
-#faces.append( face7 )
-
+#equiluminant colors
 col1 = (35/255.0, 197/255.0, 79/255.0)
 col2 = (196/255.0, 171/255.0, 3/255.0) # NOT APPENDED
 col3 = (255/255.0, 114/255.0, 75/255.0)
 col4 = (222/255.0, 134/255.0, 255/255.0)
 col5 = (0/255.0, 191/255.0, 187/255.0) 
-#col1 = (/255.0, /255.0, /255.0, 1.0)
 
 colors = []
 colors.append( col1 )
@@ -65,9 +44,6 @@ featTriangle = visual.ShapeStim( win, lineWidth=1.0, lineColor=(0.0, 0.0, 0.0), 
                          fillColor=(0.0, 0.0, 0.0), fillColorSpace='rgb',\
                          vertices=((0, 10), (8, -10), (-8, -10), (0, 10)), 
                          closeShape=True )
-                         #,\
-                         #pos=(0, 0), size=1, ori=0.0, opacity=1.0, contrast=1.0, depth=0)#,\
-                         #interpolate=True, lineRGB=None, fillRGB=None, name='', autoLog=True)
                          
 featDiamond = visual.ShapeStim( win, lineWidth=1.0, lineColor=(0.0, 0.0, 0.0), lineColorSpace='rgb',\
                          fillColor=(0.0, 0.0, 0.0), fillColorSpace='rgb',\
@@ -84,6 +60,7 @@ featArrow = visual.ShapeStim( win, lineWidth=1.0, lineColor=(0.0, 0.0, 0.0), lin
                          vertices=((0, 10), (8, 0), (4,0), (4, -10), (-4,-10), (-4, 0), (-8, 0), (0,10)), 
                          closeShape=True )
 
+#not used
 #featDrop = visual.ShapeStim( win, lineWidth=1.0, lineColor=(0.0, 0.0, 0.0), lineColorSpace='rgb',\
 #                         fillColor=(0.0, 0.0, 0.0), fillColorSpace='rgb',\
 #                         vertices=((0, 10), (7, 1), (6.9, -4), (5.65, -5.65), (4, -6.9), (0,-8), (-4, -6.9), (-5.65, -5.65), (-6.9, -4), (-7, 1), (0, 10)), 
@@ -107,15 +84,8 @@ feats.append( featDiamond )
 feats.append( featHouse )
 feats.append( featArrow )
 
-#feats.append( featStarTrek )
-#feats.append( featA )
-
 step = IMG_W/DIMX
 half = -1*IMG_W/2 #image coords are centered
-
-
-#featN = 0
-#faceN = 0
 
 PREVAIL_COL_RATIO = 0.5
 N_OF_FACES = 18
@@ -151,7 +121,6 @@ for faceN in range( N_OF_FACES ):
                         feats[featN].fillColor = colors[c]
                         feats[featN].lineColor = colors[c]
 
-                        #feats[featN].opacity = 1.0 - x*1.0/DIMX
                         feats[featN].ori = 45+ 90*featOrientation
                         
                         feats[featN].size = (sz, sz)
@@ -171,60 +140,7 @@ for faceN in range( N_OF_FACES ):
 #                   else:
 #                        keys.clear()
 
-
 #cleanup
 win.close()
 core.quit()
 
-
-"""the old code
-while( True ):
-    
-#    gabor.draw( win )
-    #select random color and remaing colors
-    cardColor = random.randint(0,3)
-    #print cardColor
-    colIdx = [0, 1, 2, 3]
-    #print colIdx
-    colIdx.remove( cardColor )
-    #print colIdx
-
-    for x in range(DIMX):
-        for y in range(DIMY):
-            feats[featN].pos = ( half+(x+1)*step, half+(y+1)*step)
-            #sz = random.random()
-       
-            val = faces[faceN].getpixel( (x,DIMY-1-y) )
-            #flip y-axis
-            sz = (255-val)/255.0
-            
-            if (random.random() <= 0.5):
-                c = cardColor
-            else:
-                c = colIdx[random.randint(0,2)]
-                
-            feats[featN].fillColor = colors[c]
-            feats[featN].lineColor = colors[c]
-
-            #feats[featN].opacity = 1.0 - x*1.0/DIMX
-            feats[featN].ori = 45+ 90*featN
-            
-            feats[featN].size = (sz, sz)
-            
-            feats[featN].draw( win )
-
-    win.flip()
-    
-    win.getMovieFrame( )
-    win.saveMovieFrames( '.\\frames\\%02d_%02d_%02d.png' % (faceN, featN, cardColor ))#+ str(G1) + '_' + str(G2) + '_' + str(L1) + '_' + str(L2) + '.png' )
-    
-    #keys = event.getKeys()
-    keys = event.waitKeys()
-    if keys:
-       if keys[0] == 'escape':
-           break
-       if keys[0] == 'space':
-           featN = (featN+1) % 6
-           faceN = (faceN+1) % 3
-
-"""
