9db0fa43b1923e64fd1aa8200cfa3899f10c1487
zenBen <benjamin.cowley@ttl.fi>
2014-06-05 17:09:47 +0300

    Testing color distribution code

--- halftone_stim_generation.py
+++ halftone_stim_generation.py
@@ -6,18 +6,20 @@ import os
 
 from PIL import Image
 
-# set FACES=0 or LETTERS=1
-type = 0
+# set FACES=0 or LETTERS=1 or TEST=2
+type = 2
 
 IMG_W = 512
 IMG_H= 512
 DIMX = 26
 DIMY = 26
 COEF = 338 # X * Y / 2
-# s = os.sep
-tystr = ['face', 'letter']
-pth = '26x26/'
-outdir = tystr[type] + 'Cards/'
+s = os.sep
+tystr = ['face', 'letter', 'test']
+pth = '26x26' + s
+outdir = tystr[type] + 'Cards' + s
+if not os.path.exists(outdir):
+    os.makedirs(outdir)
 # pth = 'C:\\Kride\\Projects\\ReKnow\\WCST\\26x26\\'
 
 #lenovo
@@ -26,14 +28,16 @@ myMon=monitors.Monitor('yoga', width=29.3, distance=40); myMon.setSizePix((3200,
 #myMon=monitors.Monitor('asus', width=37.8, distance=40); myMon.setSizePix((1920, 1080))
 win = visual.Window( size=(IMG_W, IMG_H),units='pix',fullscr=False,monitor=myMon, color=(1.0, 1.0, 1.0), colorSpace='rgb')
 
-faces = []
+imgs = []
 
-if type == 1:
+if type == 0:
     for i in range(16):
-        faces.append( Image.open(pth + 'sf' + '%02d' % (i+1,) + '.png') )   #faces
-else:
+        imgs.append( Image.open(pth + 'sf' + '%02d' % (i+1,) + '.png') )   #faces
+elif type == 1:
     for i in range(17):
-        faces.append( Image.open(pth + 'smlltr' + '%01d' % (i+1,) + '.png') )    #letters
+        imgs.append( Image.open(pth + 'smlltr' + '%01d' % (i+1,) + '.png') )    #letters
+else:
+    imgs.append( Image.open(pth + 'testicles.png') )   #TEST!
 
 #equiluminant colors
 col1 = (35/255.0, 197/255.0, 79/255.0)
@@ -97,8 +101,7 @@ step = IMG_W/DIMX
 half = -1*IMG_W/2 #image coords are centered
 
 PREVAIL_COL_RATIO = 0.5
-N_OF_FACES = 16
-N_OF_LTRS = 17
+N_OF_FACES = len(imgs)
 N_OF_FEATS = 4
 SAVE_FRAMES = True
 
@@ -110,47 +113,52 @@ for faceN in range( N_OF_FACES ):
     gt = 0  # greytotal
     for xi in range(DIMX):
         for yi in range(DIMY):
-            vali = faces[faceN].getpixel( (xi,DIMY-1-yi) )
+            vali = imgs[faceN].getpixel( (xi,DIMY-1-yi) )
             gt = gt + (255-vali)/255.0
+    print 'image gt=' + str(round(gt))
 
     for cardColor in range(4):
         colIdx = [0, 1, 2, 3]
         colIdx.remove( cardColor )
 
-        for featN in range( 4):# N_OF_FEATS ):
+        for featN in range(4):# N_OF_FEATS ):
 
             for featOrientation in range( 4 ): 
+
+                ct = 0  # color total - to collect how much of the total coloured area is dominant
 
                 for x in range(DIMX):
                     for y in range(DIMY):
 
                         feats[featN].pos = ( half+(x+1)*step, half+(y+1)*step)
-                        #sz = random.random()
                    
-                        val = faces[faceN].getpixel( (x,DIMY-1-y) )
+                        val = imgs[faceN].getpixel( (x,DIMY-1-y) )
                         #flip y-axis
                         sz = (255-val)/255.0
                         
                         PREVAIL_COL_RATIO = (sz/gt)*COEF
                         
-                        if (random.random() <= PREVAIL_COL_RATIO):
+#                        print 'size=' + str(round(sz,3)) + '; PCR=' + str(round(PREVAIL_COL_RATIO,2))
+                        
+                        if (random.random() < PREVAIL_COL_RATIO):
                             c = cardColor
+                            ct = ct + sz
                         else:
                             c = colIdx[random.randint(0,2)]
                             
-                        feats[featN].fillColor = colors[c]
-                        feats[featN].lineColor = colors[c]
-
-                        feats[featN].ori = 45+ 90*featOrientation
-                        
-                        feats[featN].size = (sz, sz)
-                        
+                        feats[featN].fillColor  = colors[c]
+                        feats[featN].lineColor  = colors[c]
+                        feats[featN].ori        = 45+ 90*featOrientation
+                        feats[featN].size       = (sz, sz)
                         feats[featN].draw( win )
 
                 win.flip()
                 if( SAVE_FRAMES ):
                     win.getMovieFrame()
                     win.saveMovieFrames( outdir + '%02d_%02d_%02d_%02d.png' % (faceN, cardColor, featN, featOrientation ))
+
+                #b=round(ct/gt)
+                print 'image ct=' + str(round(ct)) + '; balance=' + str(round(ct/gt, 1))
 
 #                keys = event.getKeys()
 #                if keys:
